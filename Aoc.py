6ccfa3fa4d94973a1ee14304dd341b4bf0e6238d
NirmalkumarNK2 <117423343+NirmalkumarNK2@users.noreply.github.com>
2024-02-28 20:51:51 +0530

    Add files via upload

---
+++ Aoc.py
@@ -0,0 +1,5 @@
+leng = float(input("Enter the length of the rectangle: "))
+wid = float(input("Enter the width of the rectangle: "))
+
+area = leng * wid
+print("The area of the rectangle is:", area)
